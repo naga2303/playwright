5b8350ef9d05c481f323dec4825f7daee1b1c3b7
nagarajan <nagarajan.r@volantetech.com>
2024-07-16 14:34:18 +0530

    allure report screenshot done

--- playwrightTesting/tests/tablewithCheckbox.spec.js
+++ playwrightTesting/tests/tablewithCheckbox.spec.js
@@ -2,6 +2,7 @@
 
 const {test} = require('@playwright/test');
 const { allure, default: AllureReporter ,AllureReporterOptions} = require('allure-playwright');
+const path = require('path');
 
 
 
@@ -13,7 +14,10 @@ test.describe("To test the dynamic table with checkbox",()=>{
         context = await browser.newContext();
         page = await context.newPage();
     })
-    test("To click the checkbox which are english as Primary Language",async()=>{
+    
+    test("To click the checkbox which are english as Primary Language",async({},testInfo)=>{
+        allure.parentSuite("Testing")
+        allure.story("Tables")
         
         await page.goto("https://cosmocode.io/automation-practice-webtable/");
         await page.waitForTimeout(5000);
@@ -29,7 +33,11 @@ test.describe("To test the dynamic table with checkbox",()=>{
                 await page.locator("//table[@id='countries']/tbody/tr["+i+"]/td[1]/input").check()
             }
         }
+        await page.screenshot({path:"table.png",fullPage:true})
+        await testInfo.attach('screenshot',{body:'table.png',contentType:'img/png'})
+        allure.attachment('table.png','tableScreenshot','img/png','png')
         await page.waitForTimeout(5000);
-        
+      
+      
     })
 })
