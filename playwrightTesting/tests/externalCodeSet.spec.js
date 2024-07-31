e43226049e9504a40db0f0c71bf536da32bcd98c
nagarajan <nagarajan.r@volantetech.com>
2024-07-31 15:44:50 +0530

    MultipleDownloads

--- playwrightTesting/tests/externalCodeSet.spec.js
+++ playwrightTesting/tests/externalCodeSet.spec.js
@@ -10,14 +10,27 @@ test("To test the external Code set",async({page})=>{
     await catalogueOfMessage.hover();
     await externalCodeSets.click();
 
-    const downloadFiles = page.waitForEvent('download');
-    await page.locator("//a[@aria-label='External Code Sets - XLSX format']").click()
-   
-   // await page.locator("//a[@aria-label='External Code Sets - JSON format']").click()
+    //const downloadFiles = page.waitForEvent('download');
+ /* await page.locator("//a[@aria-label='External Code Sets - XLSX format']").click()
+ // await page.locator("//a[@aria-label='External Code Sets - JSON format']").click()
     const download = await downloadFiles;
     await download.saveAs('C:\\Users\\nagarajan.r\\Downloads\\'+ download.suggestedFilename());
-
     await page.locator("//a[@aria-label='External Code Sets - XSD format']").click()
     const download1 = await downloadFiles;
-    await download1.saveAs('C:\\Users\\nagarajan.r\\Downloads\\'+ download.suggestedFilename());
+    await download1.saveAs('C:\\Users\\nagarajan.r\\Downloads\\'+ download.suggestedFilename());*/
+
+    const allZips = await page.locator("//a[contains(@href, '.zip')]").all()
+    const allDoc = await page.locator("//a[contains(@href, '.doc')]").all()
+    const allxlsx = await page.locator("//a[contains(@href, '.xlsx')]").all()
+
+   
+
+        for (const link of allZips) {
+            // Click the link to initiate download
+            const downloadFiles = page.waitForEvent('download');
+            await link.click();
+            const download = await downloadFiles;
+            await download.saveAs('C:\\Users\\nagarajan.r\\Downloads\\'+ download.suggestedFilename());
+                       
+        }
 })
